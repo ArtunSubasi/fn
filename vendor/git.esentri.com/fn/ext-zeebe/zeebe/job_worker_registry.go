5fc0a316519ad38a4144a41e1c10ee76528e2790
Artun Subasi <artun.subasi@esentri.com>
2019-01-23 11:05:55 +0100

    Configuration using environment vars

--- vendor/git.esentri.com/fn/ext-zeebe/zeebe/job_worker_registry.go
+++ vendor/git.esentri.com/fn/ext-zeebe/zeebe/job_worker_registry.go
@@ -7,30 +7,31 @@ import (
 	"log" // TODO log as fn logs
 )
 
-const brokerAddr = "host.docker.internal:26500"  // TODO read from a config / context (app config?)
-const loadBalancerAddr = "http://localhost:8080" // TODO read from a config / context (app config?)
-
 type JobWorkerRegistry struct {
 	// FnID is not unique without the app ID. The AppID should be used as well.
 	// But in this case, workers cannot be unregistered because the FnListener.AfterFnDelete only has the FnID as parameter
 	// TODO Contact the Fn Project. FnListener.AfterFnDelete should have more context, at least the AppID of the App in which the function lived.
 	jobWorkers map[string]worker.JobWorker // FnID -> JobWorker
+	loadBalancerAddr string
+	zeebeGatewayAddr string
 }
 
-func NewJobWorkerRegistry() JobWorkerRegistry {
+func NewJobWorkerRegistry(loadBalancerAddr string, zeebeGatewayAddr string) JobWorkerRegistry {
 	jobWorkerRegistry := JobWorkerRegistry{}
 	jobWorkerRegistry.jobWorkers = make(map[string]worker.JobWorker)
+	jobWorkerRegistry.loadBalancerAddr = loadBalancerAddr
+	jobWorkerRegistry.zeebeGatewayAddr = zeebeGatewayAddr
 	return jobWorkerRegistry
 }
 
 // Starts a great hard coded worker with a hard coded job type and a hard coded broker address
 func (jobWorkerRegistry *JobWorkerRegistry) RegisterFunctionAsWorker(fnID string, zeebeJobType string) {
-	client, err := zbc.NewZBClient(brokerAddr)
+	client, err := zbc.NewZBClient(jobWorkerRegistry.zeebeGatewayAddr)
 	if err != nil {
 		panic(err)
 	}
 	log.Printf("Creating a Zeebe job worker of type %v for function ID %v\n", zeebeJobType, fnID)
-	jobHandler := JobHandler(fnID, loadBalancerAddr)
+	jobHandler := JobHandler(fnID, jobWorkerRegistry.loadBalancerAddr)
 	// TODO Add more configuration possibilities for the worker (Poll Interval, Timeout, ...)
 	jobWorkerRegistry.jobWorkers[fnID] = client.NewJobWorker().JobType(zeebeJobType).Handler(jobHandler).PollInterval(1 * time.Second).Open()
 
--- vendor/git.esentri.com/fn/ext-zeebe/zeebe/zeebe.go
+++ vendor/git.esentri.com/fn/ext-zeebe/zeebe/zeebe.go
@@ -5,6 +5,8 @@ import (
 	"github.com/fnproject/fn/fnext"
 	"log" // TODO log as fn logs
 	"time"
+	"os"
+	"errors"
 )
 
 // Extension for Zeebe integration
@@ -15,18 +17,35 @@ func init() {
 type Zeebe struct {
 }
 
-func (e *Zeebe) Name() string {
+func (zeebe *Zeebe) Name() string {
 	return "git.esentri.com/fn/ext-zeebe/zeebe"
 }
 
-func (e *Zeebe) Setup(s fnext.ExtServer) error {
+func (zeebe *Zeebe) Setup(s fnext.ExtServer) error {
 	// The the extension should only be set up for the FnServer modes "Full" and "API Server"
 	// At the moment, checking this is not possible because the node type is not exposed in the ExtServer or Server types.
 	// This is not a big problem, because different FnServer Modes must be build separately.
 	// Therefore only the API Server must be build with the Zeebe extension. All other parts must be built without the extension.
 	log.Println("Zeebe integration setup!")
+
+	loadBalancerAddr := os.Getenv("FN_LB_URL")
+	if loadBalancerAddr == "" {
+		return errors.New("Zeebe: The load balancer address FN_LB_URL ist not configured. The zeebe extension could not start.")
+	}
+
+	apiServerAddr := os.Getenv("FN_API_SERVER_URL")
+	if apiServerAddr == "" {
+		return errors.New("Zeebe: The API server address FN_API_SERVER_URL ist not configured. The zeebe extension could not start.")
+	}
+
+	// TODO in the future, the Zeebe Gateway address could be read using the apps or the functions to increase flexibility
+	zeebeGatewayAddr := os.Getenv("FN_ZEEBE_GATEWAY_URL")
+	if zeebeGatewayAddr == "" {
+		return errors.New("Zeebe: The Zeebe Gateway address FN_ZEEBE_GATEWAY_URL ist not configured. The zeebe extension could not start.")
+	}
+
 	server := s.(*server.Server) // TODO this type assertion is hacky. ExtServer should implement the AddFnListener interface.
-	jobWorkerRegistry := NewJobWorkerRegistry()
+	jobWorkerRegistry := NewJobWorkerRegistry(loadBalancerAddr, zeebeGatewayAddr)
 	server.AddFnListener(&FnListener{&jobWorkerRegistry})
 
 	// TODO we eventually also need an App Listener. If an App gets deletes, all functions within are deleted as well.
@@ -35,15 +54,15 @@ func (e *Zeebe) Setup(s fnext.ExtServer) error {
 	// TODO Coolness factor: register a new Endpoint using the ExtServer interface which lists all registered functions and their zeebe job types
 	// so that we may show a simple UI of the job workers and their connections
 
-	go waitAndRegisterFunctions(&jobWorkerRegistry)
+	go zeebe.waitAndRegisterFunctions(&jobWorkerRegistry, apiServerAddr)
 	return nil
 }
 
-func waitAndRegisterFunctions(jobWorkerRegistry *JobWorkerRegistry) {
+func (zeebe *Zeebe) waitAndRegisterFunctions(jobWorkerRegistry *JobWorkerRegistry, apiServerAddr string) {
 	// Waiting for the REST endpoints to come up before querying for functions since the Extension Setup does not have any callback such as OnServerStarted
 	// TODO Get in touch with the Fn Project: Create a feature request, maybe a pull request
 	time.Sleep(1 * time.Second)
-	functionsWithZeebeJobType := GetFunctionsWithZeebeJobType(loadBalancerAddr)
+	functionsWithZeebeJobType := GetFunctionsWithZeebeJobType(apiServerAddr)
 	for _, fn := range functionsWithZeebeJobType {
 		jobWorkerRegistry.RegisterFunctionAsWorker(fn.fnID, fn.jobType)
 	}
