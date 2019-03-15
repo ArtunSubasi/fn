27f1723907489c6fa587d4136bcda3a607d7b681
Artun Subasi <artun.subasi@esentri.com>
2019-03-15 16:03:38 +0100

    Improve error handling and failing of jobs

--- vendor/github.com/ArtunSubasi/ext-zeebe/zeebe/job_handler.go
+++ vendor/github.com/ArtunSubasi/ext-zeebe/zeebe/job_handler.go
@@ -25,23 +25,30 @@ func JobHandler(fnID string, loadBalancerHost string) worker.JobHandler {
         logrus.Debugln("Payload:", job.Payload)
         resp, err := http.Post(invocationUrl, "application/json", bytes.NewBuffer([]byte(job.Payload)))
         if err != nil {
-            logrus.Errorf("Failed to send the post request for job %v / error: %v\n", jobKey, err)
+            logrus.Errorf("Failed to send the post request for job key %v / error: %v\n", jobKey, err)
             failJob(client, job)
             return
         }
 
-        logrus.Infoln("Function invocation successful", fnID)
+        if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+            logrus.Infof("Function invocation returned the HTTP status code %v. Job key: %v / fnID: %v\n", resp.Status, jobKey, fnID)
+        } else {
+            logrus.Infof("Function invocation returned the HTTP status code %v. Failing job. Job key: %v / fnID: %v\n", resp.Status, jobKey,fnID)
+            failJob(client, job)
+            return
+        }
 
         body, err := ioutil.ReadAll(resp.Body)
         if err != nil {
-            logrus.Errorln("Failed to read the body after invoking function:", fnID)
+            logrus.Errorf("Failed to read the body after invoking function. Failing job. Job key: %v / fnID: %v\n", jobKey, fnID)
+            failJob(client, job)
             return
         }
 
         var responseJsonObject map[string]interface{}
         err = json.Unmarshal(body, &responseJsonObject)
         if err != nil {
-            logrus.Warnln("Failed to unmarshall the response. Zeebe supports only JSON objects on root level. Response will be ignored.")
+            logrus.Warnf("Failed to unmarshall the response. Zeebe supports only JSON objects on root level. Response will be ignored. Job key: %v / fnID: %v\n", jobKey, fnID)
             logrus.Debugln("Response:", string(body))
             responseJsonObject = nil
         } else {
@@ -50,13 +57,12 @@ func JobHandler(fnID string, loadBalancerHost string) worker.JobHandler {
 
         request, err := client.NewCompleteJobCommand().JobKey(jobKey).PayloadFromObject(responseJsonObject) 
         if err != nil {
-            logrus.Warnln("failed to set the updated payload")
+            logrus.Errorf("Failed to set the updated payload. Failing job. Job key: %v / fnID: %v\n", jobKey, fnID)
             failJob(client, job)
             return
         }
     
         logrus.Println("Completed job", jobKey, "of type", job.Type)
-    
         request.Send()
     }
 }
